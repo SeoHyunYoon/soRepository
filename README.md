4f7d1c08d2739550d841e3e90bcd3d5f68476eda
SeoHyunYoon <shtjgus159@naver.com>
2021-12-11 18:53:20 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Movie App ysh
-
-React JS Fundmentals Coures 2021
---
+++ precache-manifest.a544a994bd194689b144f721d71be121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bddfe8376bbe9a66fa94d3e4df2088a",
+    "url": "/soRepository/index.html"
+  },
+  {
+    "revision": "4e35314e219a0849af34",
+    "url": "/soRepository/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "8ce00125d65d698a3ce5",
+    "url": "/soRepository/static/js/2.7e919e3b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/soRepository/static/js/2.7e919e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e35314e219a0849af34",
+    "url": "/soRepository/static/js/main.7ba6e2a0.chunk.js"
+  },
+  {
+    "revision": "467d7335a8d3a4247324",
+    "url": "/soRepository/static/js/runtime-main.033bf312.js"
+  }
+]);
